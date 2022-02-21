0c72262383c43d3e829da914d279cabd3734f08f
최우진 <dnwls6102@naver.com>
2022-02-21 12:43:38 +0900

    불필요한 코드 삭제

--- choice.py
+++ choice.py
@@ -3,41 +3,11 @@ app = Flask(__name__)
 import datas
 import random
 
-def showresult(x,y):
-    if x > y:
-        return "정답!"
-    else:
-        return "오답"
-
-def pick_another_one(x,y):
-    while True:
-        a = random.choice(list(datas.count.keys()))
-        if a == x or a == y:
-            continue
-        return a
-
-def getvalue(x):
-    return datas.count.get(x)
-
-def pickone():
-    return random.sample(list(datas.count.keys()), 2)
-
-#temp = []
 @app.route('/')
 def make_choice():
-    #global temp
-    temp = datas.pickone()
-    FirstOneValue = datas.count[temp[0]]
-    SecondOneValue = datas.count[temp[1]]
-    Fresult = showresult(FirstOneValue, SecondOneValue)
-    Sresult = showresult(SecondOneValue, FirstOneValue)
     dic = datas.count
 
-    return render_template('main.html', First = temp[0], Second = temp[1],
-                           Fresult = Fresult, Sresult = Sresult,
-                           FOV = FirstOneValue, SOV = SecondOneValue,
-                           pickAnotherOne = pick_another_one, getValue = getvalue,
-                           showResult = showresult, dic = dic)
+    return render_template('main.html', dic = dic)
 
 
 @app.route('/result')
